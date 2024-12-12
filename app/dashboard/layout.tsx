dbc36b99909fb66b409dbc7738b2322efd518332
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-12-12 21:20:05 +0530

    meta data pages

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import SideNav from "@/app/ui/dashboard/sidenav";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Acme Dashboard",
+  title: "AccMe Dashboard",
 };
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: {
     template: "%s | AccMe",
-    default: "Acme Dashboard",
+    default: "AccMe Dashboard",
   },
   description: "The official Next.js Learn Dashboard built with App Router.",
   metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
