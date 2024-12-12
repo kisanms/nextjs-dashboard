471a3bd7be402332016a655c87bb54e72d40876e
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-12-12 21:18:51 +0530

    meta data dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Acme Dashboard",
+    template: "%s | AccMe",
     default: "Acme Dashboard",
   },
   description: "The official Next.js Learn Dashboard built with App Router.",
